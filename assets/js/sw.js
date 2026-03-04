e42c9f07c50277393b859db6f48a8387cceed5c4
alejandro <fa.alvarezperez@hotmail.com>
2026-03-04 16:50:10 +0100

    Remove legacy SW migration scaffolding
    Drop inline cache purge script, skipWaiting, and clients.claim. Back to
    clean Chirpy pattern: cache-first with user-initiated refresh via toast.

--- assets/js/sw.js
+++ assets/js/sw.js
@@ -12,7 +12,6 @@ const ASSETS_TO_CACHE = [
 ];
 
 self.addEventListener('install', (event) => {
-    self.skipWaiting();
     event.waitUntil(
         caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS_TO_CACHE))
     );
@@ -22,7 +21,7 @@ self.addEventListener('activate', (event) => {
     event.waitUntil(
         caches.keys().then((keys) =>
             Promise.all(keys.filter((k) => k !== CACHE_NAME).map((k) => caches.delete(k)))
-        ).then(() => self.clients.claim())
+        )
     );
 });
 
